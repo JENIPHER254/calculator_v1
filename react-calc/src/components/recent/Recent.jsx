47937839f5e10159693f14722fc09cf4bbf6b3ba
JENIPHER <jeniperkuki@gmail.com>
2024-05-27 13:59:37 +0300

    final layout

--- react-calc/src/components/recent/Recent.jsx
+++ react-calc/src/components/recent/Recent.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 const Recent = ()=>{
     return<>
-       <div className="">Recent</div>
+       <div className="pt-3">
+           <div className="title font-semibold text-center">
+            Recent Calculations
+           </div>
+           <div className="title font-semibold fade1 text-center ">
+            123 + 345 x 0 = 0
+           </div>
+           <div className="title font-semibold fade2 text-center">
+            12 - 2 + 4 = 14
+           </div>
+       </div>
     </>
 }
 export default Recent;

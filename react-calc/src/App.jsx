fae3e0f6c09d81c2f8ee3a007cd7d05dc91006a9
JENIPHER <jeniperkuki@gmail.com>
2024-05-27 12:30:43 +0300

    side title

--- react-calc/src/App.jsx
+++ react-calc/src/App.jsx
@@ -13,7 +13,7 @@ function App() {
     <>
       <div className="items-center justify-center h-100 flex py-8">
         <div className="w-4/5 grid md:grid-flow-col grid-flow-row gap-0 h-full text-white font-serif">
-          <div className="left-side md:col-span-1 col-span-4 bg-blue2 rounded p-5">
+          <div className="left-side md:col-span-1 col-span-4 bg-blue2 rounded p-2">
             <Sidebar />
           </div>
           <div className="right-side col-span-4 flex items-center justify-center bg-blue1 rounded">
--- react-calc/src/components/sidebar/Sidebar.jsx
+++ react-calc/src/components/sidebar/Sidebar.jsx
@@ -3,7 +3,12 @@ const Sidebar = () => {
   return (
     <>
       <div className="">
-        <div className="title font-semibold text-3xl text-center mt-4">Calculated</div>
+        <div className="title ">
+          <h1 className="font-semibold text-3xl text-center mt-5">
+            Calculated
+          </h1>
+          <h1 className="text-center">Click to Select your Calculator</h1>
+        </div>
       </div>
     </>
   );

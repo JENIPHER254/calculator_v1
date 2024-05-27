ed1e2a0baaddf3d1ed1f2dcb2e35c9aca5b772e1
JENIPHER <jeniperkuki@gmail.com>
2024-05-27 13:46:16 +0300

    results

--- react-calc/src/components/results/results.jsx
+++ react-calc/src/components/results/results.jsx
@@ -1,7 +1,11 @@
 import React from "react";
-const Results = ()=>{
-    return<>
-       <div className="">Results</div>
+const Results = () => {
+  return (
+    <>
+      <div className="justify-end items-end flex my-5 mx-1">
+        <h1 className="text-4xl">124465</h1>
+      </div>
     </>
-}
+  );
+};
 export default Results;
